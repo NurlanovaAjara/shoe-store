fbd554bfe42213a70da6f41efcddcc8c502b168c
Ajar Nurlanbekova <ajara.nurlanbekova16@gmail.com>
2025-04-24 01:23:13 +0600

    Add :login and burger menu

---
+++ components/MobileMenu.tsx
@@ -0,0 +1,14 @@
+import { AlignLeft } from "lucide-react";
+function MobileMenu() {
+  return (
+    <>
+      <button>
+        <AlignLeft
+          className="hover:text-shop_dark_color md:hidden cursor-
+pointer"
+        />
+      </button>
+    </>
+  );
+}
+export default MobileMenu;
---
+++ components/Signin.tsx
@@ -0,0 +1,9 @@
+function Signin() {
+    return (
+    <button className="text-sm font-semibold text-shop_light_color cursor-
+    pointer hover:text-shop_dark_color hover-effect">
+    Login
+    </button>
+    );
+    }
+    export default Signin;
