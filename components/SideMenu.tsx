275950cabc39cec6619a3b936a46ad94c7530564
Ajar Nurlanbekova <ajara.nurlanbekova16@gmail.com>
2025-04-24 23:58:04 +0600

    Add :social media and custom hook

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -5,12 +5,17 @@ import { headerData } from "@/static-data/data";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import SocialMedia from "./SocialMedia";
+import { useOutsideClick } from "@/hooks";
+
 interface SidebarProps {
   isOpen: boolean;
   onClose: () => void;
 }
+
 const SideMenu: FC<SidebarProps> = ({ isOpen, onClose }) => {
   const pathname = usePathname();
+  const sidebarRef = useOutsideClick<HTMLDivElement>(onClose);
+
   return (
     <div
       className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white shadow-xl ${
@@ -18,6 +23,7 @@ const SideMenu: FC<SidebarProps> = ({ isOpen, onClose }) => {
       } hover-effect`}
     >
       <div
+        ref={sidebarRef}
         className="min-w-72 max-w-96 bg-black text-white h-screen p-10 border-r border-r-gray-500 flex flex-col gap-6"
       >
         <div className="flex items-center justify-between gap-5">
@@ -29,7 +35,6 @@ const SideMenu: FC<SidebarProps> = ({ isOpen, onClose }) => {
             <X />
           </button>
         </div>
-
         <div className="flex flex-col space-y-3.5 font-semibold tracking-wide">
           {headerData.map((item) => (
             <Link
@@ -43,9 +48,10 @@ const SideMenu: FC<SidebarProps> = ({ isOpen, onClose }) => {
             </Link>
           ))}
         </div>
-        <SocialMedia/>
+        <SocialMedia />
       </div>
     </div>
   );
 };
+
 export default SideMenu;
