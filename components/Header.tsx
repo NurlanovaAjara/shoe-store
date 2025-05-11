f711c076feaeb65440b1c19bb2a2766966ea3df7
Ajar Nurlanbekova <ajara.nurlanbekova16@gmail.com>
2025-05-11 19:44:40 +0600

    Add :сlerkprovider

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import { ClerkLoaded, SignedIn, UserButton } from "@clerk/nextjs";
 import CartIcon from "./CartIcon";
 import Container from "./Container";
 import FavoriteButton from "./FavoriteButton";
@@ -6,7 +7,9 @@ import Logo from "./Logo";
 import MobileMenu from "./MobileMenu";
 import SearchBar from "./SearchBar";
 import Signin from "./Signin";
-function Header() {
+import { currentUser } from "@clerk/nextjs/server";
+async function Header() {
+  const user = await currentUser();
   return (
     <header className="bg-white py-5 border-b border-b-black/20">
       <Container className="flex items-center justify-between">
@@ -17,14 +20,17 @@ start md:gap-0"
           <MobileMenu />
           <Logo />
         </div>
-
         <HeaderMenu />
-
         <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
           <SearchBar />
           <CartIcon />
           <FavoriteButton />
-          <Signin />
+          <ClerkLoaded>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            {!user && <Signin />}
+          </ClerkLoaded>
         </div>
       </Container>
     </header>
