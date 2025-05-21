c2e193073f09d2f1601ce14e9129e23b5ef6b149
Ajar Nurlanbekova <ajara.nurlanbekova16@gmail.com>
2025-05-21 18:06:42 +0600

    Add :data types

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,23 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { categoryType } from './categoryType'
+import { type SchemaTypeDefinition } from "sanity";
+import { categoryType } from "./categoryType";
+import { addressType } from "./addressType";
+import { authorType } from "./authorType";
+import { blockContentType } from "./blockContentType";
+import { blogCategoryType } from "./blogCategoryType";
+import { brandType } from "./brandTypes";
+import { orderType } from "./orderType";
+import { productType } from "./productType";
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType],
-}
+  types: [
+    categoryType,
+    addressType,
+    authorType,
+    blockContentType,
+    blogCategoryType,
+    brandType,
+    orderType,
+    productType,
+  ],
+};
+
