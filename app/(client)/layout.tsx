5e32852db6ec1538452cd72ae6beb60f5d99032d
Ajar Nurlanbekova <ajara.nurlanbekova16@gmail.com>
2025-05-21 17:01:40 +0600

    feat: setup sanity

---
+++ app/(client)/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+import Footer from "@/components/Footer";
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Shoe-store",
+    default: "Shoe-store",
+  },
+  description: " The best shoe store in the world",
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">{children}</main>
+
+        <Footer />
+      </div>
+    </ClerkProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,18 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Header from "@/components/Header";
-import { ClerkProvider } from "@clerk/nextjs";
-import Footer from "@/components/Footer";
-export const metadata: Metadata = {
-  title: {
-    template: "%s | Shoe-store",
-    default: "Shoe-store",
-  },
-  description: " The best shoe store in the world",
-};
-export default function RootLayout({
-  children,
+function RootLayout({
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className="font-poppins antialiased">
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+    return (
+        <html lang="en">
+            <body className="font-poppins antialiased">
+                {children}
+               
+            </body>
+        </html>
+    );
 }
+ 
+export default RootLayout;
+
+
