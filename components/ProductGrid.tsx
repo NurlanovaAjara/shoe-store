b83c55db8b47d99214e47887085789ea45e89a6a
Ajar Nurlanbekova <ajara.nurlanbekova16@gmail.com>
2025-05-21 22:18:06 +0600

    Add :product fetching

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,16 +1,37 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HomeTabBar from "./HomeTabBar";
 import { productType } from "@/static-data/data";
-function ProductGrid() {
-const [products, setProducts] = useState([]);
-const [loading, setLoading] = useState(false);
-const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
-return (
-<div>
+import { client } from "@/sanity/lib/client";
 
-<HomeTabBar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
-</div>
-);
+function ProductGrid() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
+  const query = `*[_type == "product" && variant==$variant] | order(name desc)
+{
+..., "categories":categories[]->title
+}`;
+  const params = { variant: selectedTab.toLowerCase() };
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await client.fetch(query, params);
+        setProducts(response);
+        console.log(response);
+      } catch (error) {
+        console.error("Product fetching error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [selectedTab]);
+  return (
+    <div>
+      <HomeTabBar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
+    </div>
+  );
 }
 export default ProductGrid;
