7aef7d8897d7133141b3ab72d3695d2288e22951
Ajar Nurlanbekova <ajara.nurlanbekova16@gmail.com>
2025-05-22 18:38:09 +0600

    Add :make header sticky

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,8 @@ import { currentUser } from "@clerk/nextjs/server";
 async function Header() {
   const user = await currentUser();
   return (
-    <header className="bg-white py-5 border-b border-b-black/20">
+    <header className="bg-white/70 py-5 border-b sticky top-0 z-50 backdrop-
+blur-md">
       <Container className="flex items-center justify-between">
         <div
           className="w-auto md:w-1/3 flex items-center gap-2.5 justify-
