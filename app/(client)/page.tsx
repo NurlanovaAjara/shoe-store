b84d22572bb72b107dcce01beb6cd06a69600afa
Ajar Nurlanbekova <ajara.nurlanbekova16@gmail.com>
2025-05-22 18:51:36 +0600

    Add :color variable

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,13 +1,16 @@
 import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
 import ProductGrid from "@/components/ProductGrid";
-export default function Home() {
-  return (
-    <Container>
-      <HomeBanner />
-      <div className="py-10">
-        <ProductGrid />
-      </div>
-    </Container>
-  );
+import { getCategories } from "@/sanity/queries";
+export default async function Home() {
+const categories = await getCategories(6);
+console.log(categories);
+return (
+<Container>
+<HomeBanner />
+<div className="py-10">
+<ProductGrid />
+</div>
+</Container>
+);
 }
