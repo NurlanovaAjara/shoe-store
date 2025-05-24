cdb80da93badc2f2f82ca900aa19b423508335ae
Ajar Nurlanbekova <ajara.nurlanbekova16@gmail.com>
2025-05-24 22:59:19 +0600

    Add :display products in cart icon

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -1,15 +1,16 @@
+"use client";
+import useStore from "@/store";
 import { ShoppingBag } from "lucide-react";
 import Link from "next/link";
-
-function CartIcon() {
+const CartIcon = () => {
+  const { items } = useStore();
   return (
     <Link href={"/cart"} className="group relative">
       <ShoppingBag className="w-5 h-5 text-black hover:text-gray-500 hover-effect" />
-      <span className="absolute -top-1 -right-1 bg-black text-white h-3.5 w-3.5 rounded-full text-[9px] flex items-center justify-center">
-        0
+      <span className="absolute -top-1 -right-1 bg-black text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center">
+        {items?.length ? items?.length : 0}
       </span>
     </Link>
   );
-}
-
+};
 export default CartIcon;
