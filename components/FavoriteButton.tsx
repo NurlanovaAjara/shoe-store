d0a35fa8c1ada51e86431c548b5c70889f0ce92b
Ajar Nurlanbekova <ajara.nurlanbekova16@gmail.com>
2025-05-24 23:05:30 +0600

    Add :display favorite products

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,15 +1,67 @@
+"use client";
+import { Product } from "@/sanity.types";
+import useStore from "@/store";
 import { Heart } from "lucide-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+const FavoriteButton = ({
+  showProduct = false,
+  product,
+}: {
+  showProduct?: boolean;
+  product?: Product | null | undefined;
+}) => {
+  const { favoriteProduct, addToFavorite } = useStore();
+  const [existingProduct, setExistingProduct] = useState<Product | null>(null);
 
-function FavoriteButton() {
+  useEffect(() => {
+    const availableItem = favoriteProduct.find(
+      (item) => item?._id === product?._id
+    );
+    setExistingProduct(availableItem || null);
+  }, [product, favoriteProduct]);
+
+  const handleFavorite = (
+    e: React.MouseEvent<HTMLSpanElement | HTMLButtonElement>
+  ) => {
+    e.preventDefault();
+    if (product?._id) {
+      addToFavorite(product).then(() => {
+        toast.success(
+          existingProduct
+            ? "Product removed successfully!"
+            : "Product added successfully!"
+        );
+      });
+    }
+  };
+
   return (
-    <Link href={"/cart"} className="group relative">
-      <Heart className="w-5 h-5 text-black hover:text-gray-500 hover-effect" />
-      <span className="absolute -top-1 -right-1 bg-black text-white h-3.5 w-3.5 rounded-full text-[9px] flex items-center justify-center">
-        0
-      </span>
-    </Link>
+    <>
+      {!showProduct ? (
+        <Link href={"/wishlist"} className="group relative">
+          <Heart className="w-5 h-5 text-black hover:text-white hover:bg-black rounded-full p-1 transition-colors" />
+          <span className="absolute -top-1 -right-1 bg-black text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center">
+            {favoriteProduct?.length ? favoriteProduct?.length : 0}
+          </span>
+        </Link>
+      ) : (
+        <button
+          onClick={handleFavorite}
+          className="group relative border border-black hover:bg-black hover:text-white p-1.5 rounded-sm transition-colors"
+        >
+          {existingProduct ? (
+            <Heart
+              fill="#000"
+              className="text-white group-hover:text-white w-5 h-5"
+            />
+          ) : (
+            <Heart className="text-black group-hover:text-white w-5 h-5" />
+          )}
+        </button>
+      )}
+    </>
   );
-}
-
+};
 export default FavoriteButton;
--- components/ProductSideMenu.tsx
+++ components/ProductSideMenu.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { Product } from "@/sanity.types";
+import useStore from "@/store";
 import { Heart } from "lucide-react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 const ProductSideMenu = ({
   product,
   className,
@@ -9,14 +12,41 @@ const ProductSideMenu = ({
   product: Product;
   className?: string;
 }) => {
+  const { favoriteProduct, addToFavorite } = useStore();
+  const [existingProduct, setExistingProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    const availableProduct = favoriteProduct?.find(
+      (item) => item?._id === product?._id
+    );
+    setExistingProduct(availableProduct || null);
+  }, [product, favoriteProduct]);
+
   const handleFavorite = (e: React.MouseEvent<HTMLDivElement>) => {
-    window.alert("Favorite button clicked");
+    e.preventDefault();
+    if (product?._id) {
+      addToFavorite(product).then(() => {
+        toast.success(
+          existingProduct
+            ? "Product removed successfully!"
+            : "Product added successfully!"
+        );
+      });
+    }
   };
+
   return (
-    <div className={cn("absolute top-2 right-2", className)}>
+    <div
+      className={cn("absolute top-2 right-2 hover:cursor-pointer", className)}
+    >
       <div
         onClick={handleFavorite}
-        className="p-2.5 rounded-full bg-white text-black hover:bg-black hover:text-white hover-effect cursor-pointer"
+        className={cn(
+          "p-2.5 rounded-full hover:bg-black hover:text-white hover-effect transition-colors",
+          existingProduct
+            ? "bg-black text-white"
+            : "bg-white text-black border border-black"
+        )}
       >
         <Heart size={15} />
       </div>
