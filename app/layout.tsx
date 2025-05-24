ca5adbf9e872a7a8692ef2b84f8051cf2d0a0232
Ajar Nurlanbekova <ajara.nurlanbekova16@gmail.com>
2025-05-24 22:50:08 +0600

    Add :implement add to cart btn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
-function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html lang="en">
-            <body className="font-poppins antialiased">
-                {children}
-               
-            </body>
-        </html>
-    );
-}
- 
+import "./globals.css";
+import { Toaster } from "react-hot-toast";
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className="font-poppins antialiased">
+        {children}
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            style: {
+              background: "#000000",
+              color: "#fff",
+            },
+          }}
+        />
+      </body>
+    </html>
+  );
+};
 export default RootLayout;
-
-
--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -3,27 +3,56 @@ import { Product } from "@/sanity.types";
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
 import { ShoppingBag } from "lucide-react";
+import useStore from "@/store";
+import toast from "react-hot-toast";
+import PriceFormatter from "./PriceFormatter";
+import QuantityButtons from "./QuantityButtons";
 interface Props {
   product: Product;
   className?: string;
 }
 const AddToCartButton = ({ product, className }: Props) => {
+  const { addItem, getItemCount } = useStore();
+  const itemCount = getItemCount(product?._id);
   const isOutOfStock = product?.stock === 0;
   const handleAddToCart = () => {
-    window.alert("Added to cart");
+    if ((product?.stock as number) > itemCount) {
+      addItem(product);
+      toast.success(
+        `${product?.name?.substring(0, 12)}... added successfully!`
+      );
+    } else {
+      toast.error("Can not add more than available stock");
+    }
   };
   return (
     <div className="w-full h-12 flex items-center">
-      <Button
-        onClick={handleAddToCart}
-        disabled={isOutOfStock}
-        className={cn(
-          "w-full bg-black text-white shadow-none border border-black font-semibold tracking-wide hover:bg-white hover:text-black hover:border-black transition-colors",
-          className
-        )}
-      >
-        <ShoppingBag /> {isOutOfStock ? "Out of Stock" : "Add to Cart"}
-      </Button>
+      {itemCount ? (
+        <div className="text-sm w-full">
+          <div className="flex items-center justify-between">
+            <span className="text-xs text-black/80">Quantity</span>
+            <QuantityButtons product={product} />
+          </div>
+          <div className="flex items-center justify-between border-t pt-1">
+            <span className="text-xs font-semibold text-black">Subtotal</span>
+            <PriceFormatter
+              amount={product?.price ? product?.price * itemCount : 0}
+              className="text-black"
+            />
+          </div>
+        </div>
+      ) : (
+        <Button
+          onClick={handleAddToCart}
+          disabled={isOutOfStock}
+          className={cn(
+            "w-full bg-black text-white shadow-none border border-black font-semibold tracking-wide hover:bg-white hover:text-black hover:border-black transition-colors",
+            className
+          )}
+        >
+          <ShoppingBag /> {isOutOfStock ? "Out of Stock" : "Add to Cart"}
+        </Button>
+      )}
     </div>
   );
 };
